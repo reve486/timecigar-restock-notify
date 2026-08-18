60914b99ae2d13d83e169eb33af319fa010eb491
reve486 <161063802+reve486@users.noreply.github.com>
2026-08-18 23:58:38 +0800

    Update config.template.js

--- site/config.template.js
+++ site/config.template.js
@@ -1,3 +1,4 @@
 window.RESTOCK_CONFIG = {
-  subscriptionEndpoint: "__SUBSCRIPTION_ENDPOINT__"
+  subscriptionEndpoint: "__SUBSCRIPTION_ENDPOINT__",
+  statusEndpoint: "https://raw.githubusercontent.com/reve486/timecigar-restock-notify/main/monitor/state.json"
 };
